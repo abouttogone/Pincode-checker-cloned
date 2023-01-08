5aa2c2a3ea48a752cf61c10bdbdb14463d04a821
impoiler <hello@suresh.im>
2023-01-08 23:00:04 +0530

    initial

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const port = 7500;
+const fs = require('fs');
+
+const pincodeData = fs.readFileSync('./data/pincodeData.json', 'utf8');
+
+function Check(pincode) {
+	return JSON.parse(pincodeData)?.filter((e) => {
+		return e.Pincode === pincode;
+	});
+}
+
+app.get('/', (req, res) => {
+	if (!req?.query?.check) {
+		return res.status(200).json({
+			status: 'running',
+			message: 'Enter the Pincode in check query to check the Pincode',
+			example: `${req?.headers?.host}?check=400004`,
+		});
+	}
+
+	res.status(200).json({
+		status: 'success',
+		data: Check(req?.query?.check),
+	});
+});
+
+app.listen(port, () => console.log('> Server is up and running on port : ' + port));
